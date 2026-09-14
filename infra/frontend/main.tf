0c1a269556a8daa877e54e5548523683bbd84344
SoONmjG2 <ecolead@naver.com>
2026-09-14 22:51:47 +0900

    refactor: separate frontend Terraform structure

--- infra/frontend/main.tf
+++ infra/frontend/main.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# 기본 AWS 리전
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+# CloudFront Free-Plan용 WAF
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket" "frontend" {
   bucket = var.frontend_bucket_name
 }
